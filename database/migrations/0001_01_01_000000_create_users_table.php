061563b361b0d0ebff99426e17b784c682684e80
Tomáš Dulovič <tomasdulovic@MacBook-Pro-uzivatela-Tomas.local>
2026-06-26 14:27:48 +0200

    Update migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('token_UNIQ')->unique()->after('password');
+            //$table->string('token_UNIQ')->unique();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
