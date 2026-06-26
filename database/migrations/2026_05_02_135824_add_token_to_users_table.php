7b6ab088a6edb0774ceb4a424132a696d28f99c4
Tomáš Dulovič <tomasdulovic@MacBook-Pro-uzivatela-Tomas.local>
2026-06-26 13:44:54 +0200

    Update migrations

--- database/migrations/2026_05_02_135824_add_token_to_users_table.php
+++ database/migrations/2026_05_02_135824_add_token_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('token_UNIQ')->unique()->after('password');
+            $table->string('token_UNIQ')->unique();
         });
     }
 
